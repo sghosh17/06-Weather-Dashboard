65b69ddf9e8e780f7efa88554a73b820d74317a7
Sampreeti Das <sampreetighosh@gmail.com>
2021-11-28 23:48:05 +0000

    initial commit for weather dashboard

---
+++ script.js
@@ -0,0 +1,7 @@
+//To do:
+
+// Identity and targert in javascript: city input, search button
+//  nedd to do a fetch request with the city input.
+// when the data returns is diaplayed on our website.
+// we need to store the previous searches in local storage
+// the we need to diaplay all the stored items
